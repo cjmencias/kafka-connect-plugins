aa7ee11b9435af2c00f630d6eae6c65654010ac0
Christian Jacob Mencias <cjmencias88@gmail.com>
2023-06-23 22:32:41 +0800

    Minor code refactoring
    Signed-off-by: Christian Jacob Mencias <cjmencias88@gmail.com>

--- src/main/java/org/cjmencias/kafka/connect/transforms/InsertFieldCurrentTimestamp.java
+++ src/main/java/org/cjmencias/kafka/connect/transforms/InsertFieldCurrentTimestamp.java
@@ -1,4 +1,4 @@
-package org.cjmencias.transforms;
+package org.cjmencias.kafka.connect.transforms;
 
 import static org.apache.kafka.connect.transforms.util.Requirements.requireMap;
 import static org.apache.kafka.connect.transforms.util.Requirements.requireStruct;
@@ -21,27 +21,27 @@ import org.apache.kafka.connect.transforms.util.SimpleConfig;
 /**
  * This transformation inserts the current system timestamp into the record payload. 
  * It is useful when you need to capture the time at which the record is processed, 
- * such as when Sink connectors consume a record. It provides information about 
+ * such as when Sink connectors consume a record. It can be used to provides information about 
  * the delay between the time the record is produced and when it is consumed.
  */
-public class InsertCurrentTimestamp<R extends ConnectRecord<R>> implements Transformation<R> {
+public class InsertFieldCurrentTimestamp<R extends ConnectRecord<R>> implements Transformation<R> {
 
-    private static final String TIMESTAMP_FIELD = "timestamp.field";
+    private static final String CONFIG_FIELD = "field";
 
     private static final String PURPOSE = "field insertion";
 
     public static final ConfigDef CONFIG_DEF = new ConfigDef().define(
-        TIMESTAMP_FIELD, ConfigDef.Type.STRING, ConfigDef.NO_DEFAULT_VALUE, 
-        ConfigDef.Importance.MEDIUM, "Field name for record timestamp.");
+        CONFIG_FIELD, ConfigDef.Type.STRING, ConfigDef.NO_DEFAULT_VALUE, 
+        ConfigDef.Importance.MEDIUM, "Field name for the current timestamp.");
 
-    private String timestampField;
+    private String field;
 
     @Override
     public void configure(Map<String, ?> props) {
         final SimpleConfig config = new SimpleConfig(CONFIG_DEF, props);
-        timestampField = config.getString(TIMESTAMP_FIELD);
-        if (timestampField == null) {
-            throw new ConfigException("No timestamp field configured");
+        field = config.getString(CONFIG_FIELD);
+        if (field == null) {
+            throw new ConfigException("No field configured");
         }
     }
 
@@ -77,7 +77,7 @@ public class InsertCurrentTimestamp<R extends ConnectRecord<R>> implements Trans
     protected R applySchemaless(R record) {
         final Map<String, Object> value = requireMap(operatingValue(record), PURPOSE);
         final Map<String, Object> updatedValue = new HashMap<>(value);
-        updatedValue.put(timestampField, System.currentTimeMillis());
+        updatedValue.put(field, System.currentTimeMillis());
         return newRecord(record, null, updatedValue);
     }
 
@@ -88,7 +88,7 @@ public class InsertCurrentTimestamp<R extends ConnectRecord<R>> implements Trans
         for (Field field : value.schema().fields()) {
             updatedValue.put(field.name(), value.get(field));
         }
-        updatedValue.put(timestampField, new Date());
+        updatedValue.put(field, new Date());
         return newRecord(record, updatedSchema, updatedValue);
     }
 
@@ -103,7 +103,7 @@ public class InsertCurrentTimestamp<R extends ConnectRecord<R>> implements Trans
         for (Field field : schema.fields()) {
             builder.field(field.name(), field.schema());
         }
-        builder.field(timestampField, Timestamp.SCHEMA);
+        builder.field(field, Timestamp.SCHEMA);
         return builder.build();
     }
 
